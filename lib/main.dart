5555e5add9db07702bbecb32ac7ba7f0597efce3
ibrahimkhalilov <ibrahim.khalilov@yahoo.com>
2023-01-11 21:11:10 -0500

    stateless widgets only, layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import './transaction.dart';
 
@@ -21,10 +22,16 @@ class MyHomePage extends StatelessWidget {
         id: 't2', title: 'New Books', amount: 99.99, date: DateTime.now()),
     Transaction(
         id: 't3',
-        title: 'Guljannat\'s backpack',
+        title: 'Guljannat\'s Backpack',
         amount: 24.99,
         date: DateTime.now())
   ];
+
+  // var titleInput;
+  // var amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +39,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -43,21 +50,69 @@ class MyHomePage extends StatelessWidget {
               child: Text("CHART!"),
             ),
           ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                        // onChanged: (val) => titleInput = val,
+                        controller: titleController,
+                        decoration: InputDecoration(labelText: 'Title')),
+                    TextField(
+                        //  onChanged: (val) => amountInput = val,
+                        controller: amountController,
+                        decoration: InputDecoration(labelText: 'Amount')),
+                    ElevatedButton(
+                      onPressed: () {
+                        print(titleController.text);
+                        print(amountController.text);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.purple,
+                        backgroundColor: Colors.transparent,
+                        elevation: 15.0,
+                      ),
+                      child: Text('Add Transaction'),
+                    )
+                  ]),
+            ),
+          ),
           Column(
             children: transactions.map((tx) {
               return Card(
                   child: Row(
                 children: <Widget>[
                   Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: EdgeInsets.all(10),
                     child: Text(
-                      tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                      '\$${tx.amount}',
                     ),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tx.title),
                       Text(
-                        tx.date.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          tx.title),
+                      Text(
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                            fontStyle: FontStyle.italic),
+                        DateFormat('yyy/MM/dd').format(tx.date),
                       )
                     ],
                   ),
