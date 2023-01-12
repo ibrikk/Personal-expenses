16a7916f056d1afacd2c3e4130f2e7385c3573a6
ibrahimkhalilov <ibrahim.khalilov@yahoo.com>
2023-01-11 23:04:59 -0500

    ListView with itemBuilder()

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
       ),
       body: SingleChildScrollView(
         child: Column(
