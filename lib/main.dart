df02bafc1dd8802d42393fdee4837b791cdd6ab1
ibrahimkhalilov <ibrahim.khalilov@yahoo.com>
2023-01-12 19:28:37 -0500

    polish

--- lib/main.dart
+++ lib/main.dart
@@ -34,15 +34,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    // Transaction(
-    //     id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    // Transaction(
-    //     id: 't2', title: 'New Books', amount: 99.99, date: DateTime.now()),
-    // Transaction(
-    //     id: 't3',
-    //     title: 'Guljannat\'s Backpack',
-    //     amount: 24.99,
-    //     date: DateTime.now())
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Books', amount: 99.99, date: DateTime.now()),
+    Transaction(
+        id: 't3',
+        title: 'Guljannat\'s Backpack',
+        amount: 24.99,
+        date: DateTime.now())
   ];
 
   List<Transaction> get _recentTransactions {
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:personal/models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -21,16 +22,41 @@ class Chart extends StatelessWidget {
         }
       }
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: null,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                  (data['day'] as String),
+                  (data['amount'] as double),
+                  totalSpending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / totalSpending),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
